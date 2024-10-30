797bd79ae27bc7d7fa74f1599a9f24a7aa8049ae
Jonmarc <jjonmarc.jj@gmail.com>
2024-10-30 12:02:17 -0400

    Added working fragments so we can navigate to the Home, Profile and Settings

--- app/src/main/java/com/example/loginapp/DashboardActivity.kt
+++ app/src/main/java/com/example/loginapp/DashboardActivity.kt
@@ -2,11 +2,44 @@ package com.example.loginapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.loginapp.databinding.ActivityDashboardBinding
+import com.example.loginapp.databinding.ActivityMainBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.menuHome -> replaceFragment(HomeFragment())
+                R.id.menuAccount -> replaceFragment(ProfileFragment())
+                R.id.menuSettings -> replaceFragment(SettingsFragment())
+
+                else ->{
+
+                }
+
+
+        }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
