9e112895fb4981ae3dbaf31ccbb55a5e08060516
AZhou <alexzhou.645@gmail.com>
2024-10-31 21:56:28 -0400

    Serialization File for requests

---
+++ app/src/main/java/com/example/loginapp/Requests.kt
@@ -0,0 +1,10 @@
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RegisterRequest(
+    val first_name: String,
+    val last_name: String,
+    val email: String,
+    val password: String
+
+)
