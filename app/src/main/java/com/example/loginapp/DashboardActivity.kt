cfe17c08676cb64e3a11fab9b25320d2a8bbcd0e
Jonmarc <jjonmarc.jj@gmail.com>
2024-11-01 11:17:22 -0400

    Updated activity_dashboard.xml to add the navigation bar has an error atm

--- app/src/main/java/com/example/loginapp/DashboardActivity.kt
+++ app/src/main/java/com/example/loginapp/DashboardActivity.kt
@@ -2,6 +2,7 @@ package com.example.loginapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.loginapp.databinding.ActivityDashboardBinding
 import com.example.loginapp.databinding.ActivityMainBinding
@@ -9,12 +10,15 @@ import com.example.loginapp.databinding.ActivityMainBinding
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(HomeFragment())
+
+        //drawerLayout = findViewById<DrawerLayout>(R.id.)
 
         binding.bottomNavigationView.setOnItemSelectedListener {
 
@@ -41,6 +45,7 @@ class DashboardActivity : AppCompatActivity() {
                 }
 
             }
+
             true
         }
 
@@ -52,5 +57,7 @@ class DashboardActivity : AppCompatActivity() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
+
     }
+
 }
