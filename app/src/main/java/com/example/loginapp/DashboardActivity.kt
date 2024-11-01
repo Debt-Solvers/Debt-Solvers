0449091680cd685a1f09dd586943ba9293516dcf
Jonmarc <jjonmarc.jj@gmail.com>
2024-11-01 07:43:57 -0400

    Updated DashboardActivity.kt to match old version to debug

--- app/src/main/java/com/example/loginapp/DashboardActivity.kt
+++ app/src/main/java/com/example/loginapp/DashboardActivity.kt
@@ -1,43 +1,20 @@
 package com.example.loginapp
 
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.core.view.GravityCompat
 import com.example.loginapp.databinding.ActivityDashboardBinding
 import com.example.loginapp.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
 
-class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class DashboardActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var binding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
-
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        if (savedInstanceState == null){
-            replaceFragment(HomeFragment())
-            navigationView.setCheckedItem(R.id.nav_home)
-        }
+        replaceFragment(HomeFragment())
 
         binding.bottomNavigationView.setOnItemSelectedListener {
 
@@ -51,19 +28,11 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
 
                 }
 
-        }
+
+            }
             true
         }
 
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.nav_home -> replaceFragment(HomeFragment())
-            R.id.nav_about -> replaceFragment(AboutFragment())
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 
     private fun replaceFragment(fragment: Fragment){
@@ -73,13 +42,4 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
-    override fun onBackPressed() {
-        super.onBackPressed()
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            onBackPressedDispatcher.onBackPressed()
-        }
-    }
-
 }
