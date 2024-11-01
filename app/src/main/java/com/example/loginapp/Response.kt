3fd0e1281642ce6e0551e4592062736cd94e55af
AZhou <alexzhou.645@gmail.com>
2024-10-31 22:59:38 -0400

    Added Request Error Response Serializable

--- app/src/main/java/com/example/loginapp/Response.kt
+++ app/src/main/java/com/example/loginapp/Response.kt
@@ -12,3 +12,15 @@ data class RegisterResponse(
 data class userData(
     val userId: String
 )
+
+@Serializable
+data class RegisterErrResponse(
+    val status: Int,
+    val message: String,
+    val errors: errorData
+)
+
+@Serializable
+data class errorData(
+    val error: String
+)
