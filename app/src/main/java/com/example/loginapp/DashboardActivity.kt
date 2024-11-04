b113ad12f7f3fac3238b50722bfd7732d85e6aca
Jonmarc <jjonmarc.jj@gmail.com>
2024-11-04 14:52:37 -0500

    Updated activity_dashboard.xml and DashboardActivity.kt to properly include the sidebar menu. Also updated themes.xml to use new themes.

--- app/src/main/java/com/example/loginapp/DashboardActivity.kt
+++ app/src/main/java/com/example/loginapp/DashboardActivity.kt
@@ -1,63 +1,79 @@
 package com.example.loginapp
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.loginapp.databinding.ActivityDashboardBinding
 import com.example.loginapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityDashboardBinding
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(HomeFragment())
 
         //drawerLayout = findViewById<DrawerLayout>(R.id.)
+        setSupportActionBar(binding.toolbar)
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
+        binding.navDrawer.setNavigationItemSelectedListener(this)
 
-            when(it.itemId){
-
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId){
                 R.id.menuHome -> replaceFragment(HomeFragment())
                 R.id.menuAccount -> replaceFragment(ProfileFragment())
                 R.id.menuSettings -> replaceFragment(SettingsFragment())
-
-                else ->{
-
-                }
-
             }
             true
         }
-        binding.navView.setNavigationItemSelectedListener{
-            when(it.itemId){
+        fragmentManager = supportFragmentManager
+        replaceFragment(HomeFragment())
 
-                R.id.nav_home -> replaceFragment(HomeFragment())
-                R.id.nav_about -> replaceFragment(AboutFragment())
-                else ->{
-
-                }
-
-            }
-
-            true
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categories", Toast.LENGTH_SHORT).show()
         }
-
     }
 
     private fun replaceFragment(fragment: Fragment){
 
-        val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> replaceFragment(HomeFragment())
+            R.id.nav_about -> replaceFragment(ProfileFragment())
+            R.id.nav_settings -> replaceFragment(SettingsFragment())
+            //R.id.nav_share -> replaceFragment()
+            //R.id.nav_logout -> replaceFragment()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
 }
