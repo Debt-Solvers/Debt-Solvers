3ab631f2960471488ecd70514832c5bbe3849c90
AZhou <alexzhou.645@gmail.com>
2024-11-05 14:17:18 -0500

    Confirmed it recieves the auth token before clearing it

--- app/src/main/java/com/example/loginapp/DashboardActivity.kt
+++ app/src/main/java/com/example/loginapp/DashboardActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.loginapp.databinding.ActivityDashboardBinding
 import com.example.loginapp.databinding.ActivityMainBinding
+import com.example.loginapp.viewmodel.DashboardViewModel
 import okhttp3.*
 import java.io.IOException
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
-//    private lateinit var logOutButton: TextView
+    private lateinit var tokenManager: TokenManager
+//    private lateinit var dashboardViewModel: DashboardViewModel by viewModels()
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var logOutButton: TextView
 //    private lateinit var expenseButton: TextView
 
-//    val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +35,34 @@ class DashboardActivity : AppCompatActivity() {
         binding.bottomNavigationView.setOnItemSelectedListener {
 
             when (it.itemId) {
-
                 R.id.menuHome -> replaceFragment(HomeFragment())
                 R.id.menuAccount -> replaceFragment(ProfileFragment())
                 R.id.menuSettings -> replaceFragment(SettingsFragment())
 
                 else -> {
-
                 }
-
-
             }
             true
+        }
+
+//        val token = intent.getStringExtra("TOKEN")
+        /*
+        requireContext() is specifically meant for use within Fragments and is not available in Activity classes.
+        In an Activity, you typically use this or applicationContext as the context instead.
+         */
+
+        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        tokenManager = TokenManager.getInstance(this)
+        val checkT = tokenManager.getToken()
+        Log.d("DashboardActivity", "Check token before clear: $checkT")
+        tokenManager.clearToken()
+//
+        val token = tokenManager.getToken()
+        if (token !=null){
+            Log.d("DashboardActivity", "Received token: $token")
+        }else{
+            Log.d("DashboardActivity", "Failed to get token $token")
         }
         /*
       Reference the logout button and set an event that when it is clicked, it does the logout function.
@@ -49,15 +71,21 @@ class DashboardActivity : AppCompatActivity() {
 //        logOutButton.setOnClickListener{
 //            logout()
 //        }
-//        expenseButton = findViewById(R.id.expenseView)
-//        expenseButton.setOnClickListener {
-//            expenseView()
-//        }
-        /*
-        Get the data from the server when you move to the dashboard
-        */
-        // fetchDataFromServer()
 
+    }
+    private fun handleLogOut(){
+        Log.d("DashboardActivity", "Entered handleLogOut")
+       val token = tokenManager.getToken()
+        if (token !=null){
+            tokenManager.clearToken()
+            Log.d("DashboardActivity", "Cleared the token it is now $token empty")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Log.d("DashboardActivity", "For some reason there is no token.")
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -67,72 +95,29 @@ class DashboardActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
-    private fun logout() {
-
-        val sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-
-        // Remove the token
-        editor.remove("authToken").apply()
-
-        //Checks to make sure if the user token is removed first before moving pages.
-        if (sharedPreferences.getString("authToken", null) == null) {
-            //If the token is removed ...
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        } else {
-            Toast.makeText(this, "Logout failed. Please try again.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun expenseView() {
-
-        val intent = Intent(this, ExpenseActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
 }
 
-
-    /*
-        We use runOnUiThread to update the UI safely when we get a successful response from fetchDataFromServer()
-     */
-//    private fun fetchDataFromServer() {
+//    private fun logout() {
+//
 //        val sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE)
-//        val token = sharedPreferences.getString("authToken", null)
+//        val editor = sharedPreferences.edit()
 //
-//        if (token != null) {
-//            val request = Request.Builder()
-//                .url("http://your-backend-url/end-point")
-//                .addHeader("Authorization", "Bearer $token")
-//                .build()
+//        // Remove the token
+//        editor.remove("authToken").apply()
 //
-//            client.newCall(request).enqueue(object : Callback {
-//                override fun onFailure(call: Call, e: IOException) {
-//                    Log.e("DashboardActivity", "Network error: ${e.message}")
-//                }
-//
-//                override fun onResponse(call: Call, response: Response) {
-//                    if (response.isSuccessful) {
-//                        val responseData = response.body?.string()
-//                        Log.d("DashboardActivity", "Data received: $responseData")
-//                    } else {
-//                        Log.e("DashboardActivity", "Authorization failed: ${response.message}")
-//                    }
-//                }
-//            })
-//        } else {
-//            /*
-//                Redirects the user to the login page if there is no token of ir the token validation fails.o
-//             */
-//            Toast.makeText(this, "Please log in again", Toast.LENGTH_SHORT).show()
+//        //Checks to make sure if the user token is removed first before moving pages.
+//        if (sharedPreferences.getString("authToken", null) == null) {
+//            //If the token is removed ...
 //            val intent = Intent(this, LoginActivity::class.java)
 //            startActivity(intent)
 //            finish()
+//        } else {
+//            Toast.makeText(this, "Logout failed. Please try again.", Toast.LENGTH_SHORT).show()
 //        }
 //    }
+
+
+
 
 
 
