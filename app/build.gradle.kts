7ab9a0e92f39c6fdb74c95b2399a7c0f8918588c
Jonmarc <jjonmarc.jj@gmail.com>
2024-11-06 11:36:03 -0500

    Updated the expense page which uses Recycler View to store items and updated the stats fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.preference)
+    implementation(libs.mpandroidchart)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
