92e0239c680c1a3b159a6c98c118dfd4dea330c7
AZhou <alexzhou.645@gmail.com>
2024-11-06 22:49:13 -0500

    Updated to get userInfoData to appear on Dashboard but still need to try and test it on other pages/fragments using viewModels

--- app/src/main/java/com/example/loginapp/DashboardActivity.kt
+++ app/src/main/java/com/example/loginapp/DashboardActivity.kt
@@ -10,18 +10,14 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.example.loginapp.databinding.ActivityDashboardBinding
-import com.example.loginapp.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 import com.example.loginapp.viewmodel.DashboardViewModel
 import com.example.loginapp.viewmodel.DashboardViewModelFactory
-import okhttp3.*
-import java.io.IOException
+
 
 class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -37,7 +33,13 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
 
         //drawerLayout = findViewById<DrawerLayout>(R.id.)
         setSupportActionBar(binding.toolbar)
-        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.open_nav,
+            R.string.close_nav
+        )
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
@@ -45,7 +47,7 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
 
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when(item.itemId){
+            when (item.itemId) {
                 R.id.menuHome -> replaceFragment(HomeFragment())
                 R.id.menuProfile -> replaceFragment(ProfileFragment())
                 R.id.menuSettings -> replaceFragment(SettingsFragment())
@@ -59,17 +61,15 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         binding.fab.setOnClickListener {
             Toast.makeText(this, "Categories", Toast.LENGTH_SHORT).show()
         }
-        /*
-        requireContext() is specifically meant for use within Fragments and is not available in Activity classes.
-        In an Activity, you typically use this or applicationContext as the context instead.
-         */
+
         tokenManager = TokenManager.getInstance(this)
-        dashboardViewModel = ViewModelProvider(this, DashboardViewModelFactory(tokenManager)).get(DashboardViewModel::class.java)
+        dashboardViewModel = ViewModelProvider(
+            this,
+            DashboardViewModelFactory(tokenManager)
+        ).get(DashboardViewModel::class.java)
+
         dashboardViewModel.logoutSuccess.observe(this) { success ->
             if (success) {
-                Log.d("DashboardActivity", "Success sent to frontEndUI, success = $success")
-                val checkToken = tokenManager.getToken()
-                Log.d("DashboardActivity", "CheckToken status before moving to loginActivity: $checkToken")
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
                 finish()
@@ -77,92 +77,27 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
                 Toast.makeText(this, "Logout failed", Toast.LENGTH_SHORT).show()
             }
         }
-
-
-//        tokenManager = TokenManager.getInstance(this)
-//        val checkT = tokenManager.getToken()
-//        Log.d("DashboardActivity", "Check token before clear: $checkT")
-
+        dashboardViewModel.userData.observe(this) { result ->
+            when (result) {
+                is DashboardViewModel.UserDataResult.Success -> {
+                    Log.d("DashboardViewModel", "Managed to get userData from get Request. $result")
+                }
+                is DashboardViewModel.UserDataResult.Error -> {
+                    Log.d("DashboardViewModel", "Failed to get UserData---- $result")
+                }
+            }
+        }
+        dashboardViewModel.getUserData()
     }
-//    private fun handleLogOut(){
-//        Log.d("DashboardActivity", "Entered handleLogOut")
-//       val token = tokenManager.getToken()
-//        Log.d("DashboardActivity", "Check token before clear: $token")
-//        if (token !=null){
-//            tokenManager.clearToken()
-//
-//            val clearedToken = tokenManager.getToken()
-//            Log.d("DashboardActivity", "tokenCleared Successfully: $clearedToken")
-//            val intent = Intent(this, LoginActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        } else {
-//            Log.d("DashboardActivity", "For some reason there is no token.")
-//        }
-//    }
 
-//    private fun handleLogOut() {
-//        Log.d("DashboardActivity", "Entered handleLogOut")
-//        val token = tokenManager.getToken()
-//        Log.d("DashboardActivity", "Check token before clear: $token")
-//
-//        if (token != null) {
-//            // Send POST request to /logout endpoint
-//            val backEndURL = "http://10.0.2.2:8080/api/v1/logout"
-//
-//            // Create the request with the token in the Authorization header
-//            val request = Request.Builder()
-//                .url(backEndURL)
-//                .post(RequestBody.create(null, ByteArray(0)))  // Empty body for logout request
-//                .addHeader("Authorization", "Bearer $token")
-//                .build()
-//
-//            // Perform the async HTTP request to log out
-//            client.newCall(request).enqueue(object : Callback {
-//                override fun onFailure(call: Call, e: IOException) {
-//                    Log.e("DashboardActivity", "Logout failed: ${e.message}")
-//                    // Handle failure if necessary (e.g., show an error message)
-//                }
-//
-//                override fun onResponse(call: Call, response: Response) {
-//                    if (response.isSuccessful) {
-//                        Log.d("DashboardActivity", "Logout successful")
-//
-//                        // Clear the token after successful logout
-//                        tokenManager.clearToken()
-//
-//                        // Check if token is cleared
-//                        val clearedToken = tokenManager.getToken()
-//                        Log.d("DashboardActivity", "tokenCleared Successfully: $clearedToken")
-//
-//                        // Redirect to LoginActivity after logout
-//                        val intent = Intent(this@DashboardActivity, LoginActivity::class.java)
-//                        startActivity(intent)
-//                        finish()
-//                    } else {
-//                        Log.e("DashboardActivity", "Logout failed: ${response.message}")
-//                        // Handle failure if necessary (e.g., show an error message)
-//                    }
-//                }
-//            })
-//        } else {
-//            Log.d("DashboardActivity", "For some reason there is no token.")
-//            // Optionally, you can handle the case where there's no token (maybe show an error message)
-//        }
-//    }
-
-
-
-    fun replaceFragment(fragment: Fragment){
-
+    fun replaceFragment(fragment: Fragment) {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.nav_home -> replaceFragment(HomeFragment())
             R.id.nav_about -> replaceFragment(ProfileFragment())
             R.id.nav_settings -> replaceFragment(SettingsFragment())
@@ -174,11 +109,10 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
     }
 
     override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
     }
-
 }
--- app/src/main/java/com/example/loginapp/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/example/loginapp/viewmodel/DashboardViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.loginapp.viewmodel
-
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.loginapp.UserDataResponse
 import com.example.loginapp.TokenManager
+import kotlinx.serialization.json.Json
 import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 
 class DashboardViewModel(private val tokenManager: TokenManager) : ViewModel() {
@@ -13,42 +15,90 @@ class DashboardViewModel(private val tokenManager: TokenManager) : ViewModel() {
     private val _logoutSuccess = MutableLiveData<Boolean>()
     val logoutSuccess: LiveData<Boolean> get() = _logoutSuccess
 
+    private val _userData = MutableLiveData<UserDataResult>()
+    val userData: LiveData<UserDataResult> get() = _userData
+
+    sealed class UserDataResult {
+        data class Success(val response: UserDataResponse) : UserDataResult()
+        data class Error(val errResponse: UserDataResponse) : UserDataResult()
+    }
+
     private val client = OkHttpClient()
 
     fun logout() {
-        Log.d("DashboardActivity", "Inside Logout function")
         val token = tokenManager.getToken()
-        Log.d("DashboardActivity", "Check token: $token")
         if (token != null) {
             val backEndURL = "http://10.0.2.2:8080/api/v1/logout"
             val request = Request.Builder()
                 .url(backEndURL)
-                .post(RequestBody.create(null, ByteArray(0)))  // Empty body for logout request
+//                .post(RequestBody.create(null, ByteArray(0)))  // Empty body for logout request
+                .post(ByteArray(0).toRequestBody())
                 .addHeader("Authorization", "Bearer $token")
                 .build()
 
             client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
-                    Log.d("DashboardActivity", "Failed to send logout request")
                     _logoutSuccess.postValue(false)  // Notify failure
                 }
 
                 override fun onResponse(call: Call, response: Response) {
                     if (response.isSuccessful) {
-                        Log.d("DashboardActivity", "Inside Response Success: $response")
                         tokenManager.clearToken()  // Clear the token after successful logout
-                        val clearedToken = tokenManager.getToken()
-                        Log.d("DashboardActivity", "Check if token is cleared: $clearedToken")
+//                        val clearedToken = tokenManager.getToken()
+//                        Log.d("DashboardActivity", "Check if token is cleared: $clearedToken")
                         _logoutSuccess.postValue(true)  // Notify success
                     } else {
-                        Log.d("DashboardActivity", "Sent request but response is a fail.")
                         _logoutSuccess.postValue(false)  // Notify failure
                     }
                 }
             })
         } else {
             _logoutSuccess.postValue(false)  // Notify that there's no token
-            Log.d("DashboardActivity", "There's no token.")
+        }
+    }
+
+    fun getUserData() {
+        val token = tokenManager.getToken()
+        if (token != null) {
+            val backEndURL = "http://10.0.2.2:8080/api/v1/user"
+            val request = Request.Builder()
+                .url(backEndURL)
+                .get()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("DashboardViewModel", "Failed to retrieve user data: ${e.message}")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val responseBody = response.body?.string()
+
+                    if (responseBody.isNullOrEmpty()) {
+                        Log.e("DashboardViewModel", "responseBody is empty or null")
+                        return
+                    }
+
+                    if (response.isSuccessful) {
+                        try {
+                            val userResponse = Json.decodeFromString<UserDataResponse>(responseBody)
+                            _userData.postValue(UserDataResult.Success(userResponse))
+                        } catch (e: Exception) {
+                            Log.e("DashboardViewModel", "Error parsing successful response: ${e.message}")
+                        }
+                    } else {
+                        try {
+                            val errResponse = Json.decodeFromString<UserDataResponse>(responseBody)
+                            _userData.postValue(UserDataResult.Error(errResponse))
+                        } catch (e: Exception) {
+                            Log.e("DashboardViewModel", "Error parsing error response: ${e.message}")
+                        }
+                    }
+                }
+            })
+        } else {
+            Log.d("DashboardViewModel", "No token found for authorization.")
         }
     }
 }
