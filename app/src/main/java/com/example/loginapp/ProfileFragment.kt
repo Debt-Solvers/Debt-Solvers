64473ac78a211da4c158b161d14f585bf62693c9
AZhou <alexzhou.645@gmail.com>
2024-11-11 01:02:22 -0500

    Tested to check if user data is obtained in fragments

--- app/src/main/java/com/example/loginapp/ProfileFragment.kt
+++ app/src/main/java/com/example/loginapp/ProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.example.loginapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.loginapp.viewmodel.SharedViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +25,9 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var sharedViewModel: SharedViewModel
+    private lateinit var profileNameTextView: TextView
+    private lateinit var profileEmailTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,5 +63,22 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    // Use onViewCreated to observe user data when the fragment view is available
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        profileNameTextView = view.findViewById(R.id.textView)
+        profileEmailTextView = view.findViewById(R.id.textView2)
+
+
+
+        // Observe userData LiveData to access the user data after the view is created
+        sharedViewModel.userData.observe(viewLifecycleOwner, Observer { userData ->
+            Log.d("SettingsFragment", "check password data $userData")
+            profileNameTextView.text = userData?.data?.first_name + " " + userData?.data?.last_name
+            profileEmailTextView.text = userData?.data?.email
+        })
     }
 }
